2fb8a4dcc50619ccd7e0c63e49f145ffdf1034ce
rohini jadhav <jrohinib@gmail.com>
2013-09-23 12:27:19 +0530

    Update add_job.php

--- add_job.php
+++ add_job.php
@@ -3,14 +3,13 @@
 	error_reporting(~1);
 
 	require_once('php-sql-parser.php');
-
 	require_once('php-sql-creator.php');
 ?>
 <html>
 <head>
 	<title>Add Job</title>
-	<script type="text/javascript" src="jquery-1.8.0.min.js"></script>
-	<script type="text/javascript" src="jquery.validate.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
 	<script type="text/javascript">
 	function getValue()
  	 {
@@ -30,11 +29,12 @@
 <body>
 <?php
 
-	$con = mysqli_connect("localhost", "root", "root", "scheduler");
-	
+	$con = mysqli_connect("localhost", "root", "root", "scheduler") or die("Error in Connection:".mysqli_error($con));
+		
 	$sql= "show databases";
 	
-	$result = mysqli_query($con,$sql);
+	$result = mysqli_query($con,$sql) or die("Error in Query:".mysqli_error($con));
+	
 ?>
 	<h2 align="center">Add Job</h2><hr>
 	<form action="#" method="POST">
@@ -100,21 +100,7 @@ if(isset($_POST['submit']))
 	}
 
 	set_exception_handler('exception_handler');
-	
-	function check_for_fatal()
-	{
-    	$error = error_get_last();
-    	if ( $error["type"] == E_ERROR )
-        	log_error( $error["type"], $error["message"], $error["file"], $error["line"] );
-        echo "error";
-	}
-
-	register_shutdown_function( "check_for_fatal" );
-	set_error_handler( "log_error" );
-	set_exception_handler( "log_exception" );
-	ini_set( "display_errors", "off" );
-	error_reporting( E_ALL );
-	
+		
 	try
 	{
 		if((!$parser) OR (!$creator) OR (!$query))
@@ -122,20 +108,21 @@ if(isset($_POST['submit']))
 			 throw new Exception("Error in query");
 		}
 
+	$sql_insert="INSERT INTO jobschedule (DBName,JobName) VALUES ('$db','$job')";
+	
+	mysqli_query($con,$sql_insert) or die("Error in insert".mysqli_error($con));	
+
 	$sql_c = "DROP PROCEDURE IF EXISTS $db.$job; CREATE PROCEDURE $db.$job() BEGIN $query; END;";
 
-	mysqli_multi_query($con,$sql_c) or die(mysqli_error()."error");
-	
+	mysqli_multi_query($con,$sql_c) or die("Error in Create Procedure:".mysqli_error($con));
+
 	header('Location:batches.php');	
 	}
-
 	catch(Exception $e)
   	{
   		echo 'Message: ' .$e->getMessage();
   	}
-	//print_r($parsed);
-
-
+	
 	
 }
 
